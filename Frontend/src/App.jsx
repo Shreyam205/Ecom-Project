2191b9dfeed15e1019a11805a8a05a7240ea46f5
Shreyam Sharma <rajroy10008@gmail.com>
2026-01-06 00:27:08 +0530

    testing the axios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from './api/axiosconfig'
 
 const App = () => {
+  const getproducts = async () => {
+    try {
+      const res = await axios.get("/products")
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getproducts()
+  }, [])
+  
   return (
     <div>App</div>
   )

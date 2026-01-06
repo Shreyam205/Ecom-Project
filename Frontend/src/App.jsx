676564499caa462f70dab78cecd9d9ac0661fc9f
Shreyam Sharma <rajroy10008@gmail.com>
2026-01-07 00:10:35 +0530

    started using Redux

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect } from 'react'
 import axios from './api/axiosconfig'
+import { asyncgetusers } from "./store/userActions";
+import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
-  const getproducts = async () => {
-    try {
-      const res = await axios.get("/products")
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // const getproducts = async () => {
+  //   try {
+  //     const res = await axios.get("/products")
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  const data = useSelector((state) => state  );
+  console.log(data);
+  
+  const dispatch = useDispatch();
 
   useEffect(()=>{
-    getproducts()
+    // getproducts()
+    dispatch(asyncgetusers())
   }, [])
   
   return (
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
+import store from './store/store.jsx'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
-    <ToastContainer />
+    <Provider store={store}>
+      <App />
+      <ToastContainer />
+    </Provider>
   </BrowserRouter>
 )

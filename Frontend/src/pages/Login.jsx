196cae28878471cee481ac7c6110134330f608fa
Shreyam Sharma <rajroy10008@gmail.com>
2026-01-08 00:33:37 +0530

    working on Login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex justify-center items-center mt-10'>
+      <form className='flex flex-col lg:w-1/2 w-full gap-5'>
+        <input
+          type="text"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Username' />
+        <input
+          type="text"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Email Address' />
+        <input
+          type="text"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Password' />
+      </form>
+    </div>
   )
 }
 

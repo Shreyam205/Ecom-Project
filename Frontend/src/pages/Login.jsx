4954a440b3300cff71ff428ad9253528c17bf62c
Shreyam Sharma <rajroy10008@gmail.com>
2026-01-08 21:52:47 +0530

    added the login and register pages

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
+import { nanoid } from "nanoid"
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+  const { register, reset, handleSubmit } = useForm()
+
+  const LoginHandler = (user) => {
+    console.log(user);
+  }
+  
   return (
     <div className='flex justify-center items-center mt-10'>
-      <form className='flex flex-col lg:w-1/2 w-full gap-5'>
+      <form onSubmit={handleSubmit(LoginHandler)}
+        className='flex flex-col lg:w-1/2 w-full gap-5'>
         <input
-          type="text"
-          className='mb-3 outline-0 border-b p-2 text-xl'
-          placeholder='Enter Username' />
-        <input
-          type="text"
+        {...register("email")}
+          type="email"
           className='mb-3 outline-0 border-b p-2 text-xl'
           placeholder='Enter Email Address' />
         <input
-          type="text"
+        {...register("password")}
+          type="password"
           className='mb-3 outline-0 border-b p-2 text-xl'
           placeholder='Enter Password' />
+        <button className='bg-blue-500 p-2 rounded-md'>Login</button>
+        <p className='flex justify-end gap-5'>Don't have an account? 
+          <Link className='text-blue-500' to="/register">Register</Link>
+        </p>
       </form>
     </div>
   )
---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,42 @@
+import { useForm } from 'react-hook-form'
+import { nanoid } from "nanoid"
+import { Link } from 'react-router-dom'
+
+const Register = () => {
+
+  const { register, reset, handleSubmit } = useForm()
+
+  const RegisterHandler = (user) => {
+    user.id = nanoid()
+    console.log(user);
+  }
+  
+  return (
+    <div className='flex justify-center items-center mt-10'>
+      <form onSubmit={handleSubmit(RegisterHandler)}
+        className='flex flex-col lg:w-1/2 w-full gap-5'>
+        <input
+          {...register("username")}
+          type="text"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Username' />
+        <input
+        {...register("email")}
+          type="email"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Email Address' />
+        <input
+        {...register("password")}
+          type="password"
+          className='mb-3 outline-0 border-b p-2 text-xl'
+          placeholder='Enter Password' />
+        <button className='bg-blue-500 p-2 rounded-md'>Register</button>
+        <p className='flex justify-end gap-5'>Already have an account? 
+          <Link className='text-blue-500' to="/login">Login</Link>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Register

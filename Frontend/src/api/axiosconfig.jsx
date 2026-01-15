304203081193e63971f4fe595d5d31173a8a66c2
Shreyam Sharma <rajroy10008@gmail.com>
2026-01-15 23:09:35 +0530

    mobile api calling fixes

--- Frontend/src/api/axiosconfig.jsx
+++ Frontend/src/api/axiosconfig.jsx
@@ -4,4 +4,6 @@ const instance = axios.create({
     baseURL: "http://localhost:3000/",
 })
 
+// change localhost -> http://192.168.0.10*:3000/ for mobile view IP according to your pc
+
 export default instance;

1eee4296f51d6522dbb8d9a46a6c28fadb9f895b
Shreyam Sharma <rajroy10008@gmail.com>
2026-02-03 16:38:29 +0530

    added AuthWrapper

--- Frontend/src/routes/Mainroutes.jsx
+++ Frontend/src/routes/Mainroutes.jsx
@@ -7,20 +7,39 @@ import CreateProduct from "../pages/admin/CreateProduct";
 import ProductDetail from "../pages/admin/ProductDetail";
 import PageNotFound from '../pages/PageNotFound'
 import UserProfile from "../pages/user/UserProfile";
+import AuthWrapper from "./AuthWrapper";
 
 const Mainroutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/products" element={<Products/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-        <Route path="/admin/create-product" element={<CreateProduct/>}/>
-        <Route path="/user-profile" element={<UserProfile/>}/>
-        <Route path="/product/:id" element={<ProductDetail/>}/>
+      <Route
+        path="/admin/create-product"
+        element={
+          <AuthWrapper>
+            <CreateProduct />
+          </AuthWrapper>
+        } />
+      <Route
+        path="/user-profile"
+        element={
+          <AuthWrapper>
+            <UserProfile />
+          </AuthWrapper>
+        } />
+      <Route
+        path="/product/:id"
+        element={
+          <AuthWrapper>
+            <ProductDetail />
+          </AuthWrapper>
+        } />
 
-        <Route path="*" element={<PageNotFound/>}/>
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
 }
